e0d6ddff46246ac9f61c7515a72c7d2d402db1b5
Ajay Padwal <ajaypadwal@Ajays-MacBook-Air.local>
2022-02-21 04:06:42 +0530

    handleMessage handlePostback callSendApi callSendAPIWithTemplate done

--- src/controllers/chatBotControllers.js
+++ src/controllers/chatBotControllers.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+const request = require('request');
+const moment = require('moment');
+const { isDateValid, getNumberOfDaysLeftForBirthday } = require("./helpers");
+
+
 const getHomePage = (req, res) => {
     res.send("Hi, welcome to thhe assessment");
 }
@@ -43,6 +49,14 @@ const postWebhook = (req, res) => {
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
 
+            // Get sender psid
+            const senderPsid = webhook_event.sender.id;
+            if(webhook_event.message) {
+                handleMessage(senderPsid, webhook_event.message);
+            } else if(webhook_event.postback) {
+                handlePostback(senderPsid, webhook_event.postback);
+            }
+
         });
 
         // Return a '200 OK' response to all events
@@ -53,6 +67,125 @@ const postWebhook = (req, res) => {
         res.sendStatus(404);
     }
 }
+
+function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    // Do calculations based on the postback payload
+    if (payload.toLowerCase().includes('yes')) {
+        
+        const validDate = payload.split(',')
+            .map(item => item.trim())
+            .filter(item => isDateValid(item))
+
+        const daysLeftForBirthday = getNumberOfDaysLeftForBirthday(validDate[0]);
+
+        response = `There are ${daysLeftForBirthday} day(s) left until your next birthday`;
+    } else if (payload === 'no') {
+        response = "Bye! Nice chatting with you! Goodbye";
+    }
+    // Send the message to acknowledge the postback
+    callSendApi(sender_psid, response);
+}
+
+const handleMessage = (senderPsid, message) => {
+    let response;
+    console.log(message, 'xxxxxx');
+    if(message && message.text) {
+        const isDateAvailable = message.text.split(',')
+        .map(item => item.trim())
+        .some(item => isDateValid(item));
+
+        if(message.text.toLowerCase() === 'hi') {
+            response = 'Hi, may I know your name and birth-date?\neg: John Smith, YYYY-MM-DD';
+            callSendApi(senderPsid, response);
+        } else if(isDateAvailable) {
+            const validDate = message.text.split(',')
+            .map(item => item.trim())
+            .filter(item => isDateValid(item))
+            callSendAPIWithTemplate(senderPsid, validDate[0]);
+        } else {
+            response = 'I did not understand. Please type "hi" to start the conversation.';
+            callSendApi(senderPsid, response);
+        }
+    } 
+}
+
+const callSendApi = (senderPsid, response) => {
+    // Construct the response body
+    let requestBody = {
+        "recipient": {
+            "id": senderPsid
+        },
+        "message": { "text": response }
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v7.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": requestBody
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+let callSendAPIWithTemplate = (senderPsid, birthdate) => {
+    // document fb message template
+    // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
+    let body = {
+        "recipient": {
+            "id": senderPsid
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": `Want to know how many days till your birthday?`,
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": 'https://thumbs.dreamstime.com/b/happy-birthday-cupcake-celebration-message-160558421.jpg',
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": `yes,${birthdate}`,
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+        "method": "POST",
+        "json": body
+    }, (err, res, body) => {
+        if (!err) {
+            // console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
 
 module.exports = {
     getHomePage,
---
+++ src/controllers/helpers.js
@@ -0,0 +1,38 @@
+const moment = require('moment');
+
+const isDateValid = (date) => {
+    console.log(date, 'zzzzzzzz');
+    return moment(date, 'DD/MM/YYYY', true).isValid()
+        || moment(date, 'DD-MM-YYYY', true).isValid()
+        || moment(date, 'YYYY/MM/DD', true).isValid()
+        || moment(date, 'YYYY-MM-DD', true).isValid()
+        || moment(date, 'MM/DD/YYYY', true).isValid()
+        || moment(date, 'MM-DD-YYYY', true).isValid();
+};
+
+const getNumberOfDaysLeftForBirthday = (birthDate) => {
+    const day = moment(birthDate, 'YYYY-MM-DD').date();
+    const month = moment(birthDate, 'YYYY-MM-DD').month() + 1;
+
+    const currentDate = moment().format('YYYY-MM-DD');
+    const currentYear = moment().year();
+
+    const birthdateForCurrentYear = moment(`${currentYear}-${month}-${day}`, 'YYYY-MM-DD');
+    const birthDateForNextYear = moment(`${currentYear + 1}-${month}-${day}`, 'YYYY-MM-DD');
+
+    const daysLeftForCurrentYearBirthday = birthdateForCurrentYear.diff(currentDate, 'days');
+    const daysLeftForNextYearBirthday = birthDateForNextYear.diff(currentDate, 'days');
+
+    let daysLeftForBirthday;
+    if(daysLeftForCurrentYearBirthday > 0) {
+        daysLeftForBirthday = daysLeftForCurrentYearBirthday;
+    } else {
+        daysLeftForBirthday = daysLeftForNextYearBirthday;
+    }
+    return daysLeftForBirthday;
+}
+
+module.exports = {
+    isDateValid,
+    getNumberOfDaysLeftForBirthday
+}
