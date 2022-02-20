3776673a58798aee8432eac76da7adc3b5df32d8
Ajay Padwal <ajaypadwal@Ajays-MacBook-Air.local>
2022-02-21 01:38:59 +0530

    initial get route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send("Hi ajay")
+})
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
