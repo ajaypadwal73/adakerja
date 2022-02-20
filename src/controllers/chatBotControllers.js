38c56c4fb5181d1f70c199ac71593c531df6fc9c
Ajay Padwal <ajaypadwal@Ajays-MacBook-Air.local>
2022-02-21 02:00:25 +0530

    getWebhook done

--- src/controllers/chatBotControllers.js
+++ src/controllers/chatBotControllers.js
@@ -3,7 +3,29 @@ const getHomePage = (req, res) => {
 }
 
 const getWebhook = (req, res) => {
-    res.send("Hi, welcome from getWebhook");
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN;
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
 }
 
 const postWebhook = (req, res) => {
